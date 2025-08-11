e3ae4bab5337e175276eff6cb72ebf8e4ed68ad5
Gonzabu6 <gentange@hotmail.com>
2025-08-11 19:47:22 +0900

    Revert "Debug: Simplify grab logic for testing"

--- prototype/stage.js
+++ prototype/stage.js
@@ -10,7 +10,6 @@ const Stage = (() => {
         let targetBox, goalZone, fanVent;
 
         switch (stageId) {
-            /*
             case 3: {
                 // --- Stage 3: Breakable Wall ---
                 const wallStack = Matter.Composites.stack(width / 2, height - 250, 4, 6, 0, 0, (x, y) => {
@@ -39,7 +38,6 @@ const Stage = (() => {
                 Matter.Composite.add(world, [targetBox, goalZone]);
                 break;
             }
-            */
             case 1:
                 // --- Stage 1: The Wall ---
                 targetBox = Matter.Bodies.rectangle(450, height - 65, 60, 60, {
